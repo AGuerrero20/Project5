476a6d05ee54ecb1d2f4130e87364077db5f6212
AGuerrero20 <alexander.guerrero@patelhs.org>
2021-02-19 21:30:39 +0000

    added a second method and some println instructions

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    
+    Scanner sides = new Scanner(System.in);
+    welcome();
+    sum(sides);
   }
+
+   public static void welcome(){
+    System.out.println("Welcome to the volume calculator! Type in the number of sides you have");
+  }
+
   public static void sum(int n){
   if(n==0){
-    
+    System.out.println("Your volume is " + );
     }else{
+    
     n--;
     }
   }
