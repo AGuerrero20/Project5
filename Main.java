20863ef28833dc2b825aff0e61bfb67e961091d0
AGuerrero20 <alexander.guerrero@patelhs.org>
2021-02-23 15:41:52 +0000

    changed the if/else statement and moved the welcome message

--- Main.java
+++ Main.java
@@ -2,21 +2,32 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    Scanner sides = new Scanner(System.in);
-    welcome();
-    sum(sides);
-  }
-
-   public static void welcome(){
-    System.out.println("Welcome to the volume calculator! Type in the number of sides you have");
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Welcome to the volume calculator!");
+    sum(3);
   }
 
   public static void sum(int n){
   if(n==0){
-    System.out.println("Your volume is " + );
-    }else{
-    
+    System.out.println("Your volume is " + vol);
+    }if(n==1){
+    System.out.println("Type in the width.");
+    Scanner length = new Scanner(System.in);
+
     n--;
+    sum();
+    }if(n==2){
+    System.out.println("Type in the height.");
+    Scanner length = new Scanner(System.in);
+
+    n--;
+    sum();
+    }if(n==3){
+    System.out.println("Type in the length.");
+    Scanner length = new Scanner(System.in);
+
+    n--;
+    sum();
     }
   }
 }
